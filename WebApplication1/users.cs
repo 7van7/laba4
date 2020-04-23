3cca709dbf375162d57518a2aea7af3e14e63517
mkrtchyan_van mail.ru <mkrtchyan_van@mail.ru>
2020-04-23 19:08:20 +0700

    a

---
+++ WebApplication1/users.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class users
+    {
+
+		private int _id;
+
+		public int id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+
+		private string _login;
+
+		public string login
+		{
+			get { return _login; }
+			set { _login = value; }
+		}
+		private string _password;
+
+		public string password
+		{
+			get { return _password; }
+			set { _password = value; }
+		}
+
+	}
+}
